cc5623409b0c77055c0356f2d61667e7ef76b04b
kkb568 <b.koome@alustudent.com>
2024-09-04 14:17:59 +0300

    Change from h2 to h1 heading

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,21 +1,21 @@
-import Brian from '../../images/Brian.jpeg';
-import './About.scss'
+import Brian from "../../images/Brian.jpeg";
+import "./About.scss";
 
 export default function About() {
-    return (
-        <div className="about-content">
-            <img src={Brian}/>
-            <div className="about">
-                <h2>About me</h2>
-                <p>My name is Brian Koome from Nairobi, Kenya
-                    and I completed my studies at 
-                    African Leadership College studying Bsc. in Computing.</p>
-                <p>I am versatile and skilled in UX design, front-end
-                    development with HTML, CSS, SCSS,
-                    JavaScript and Reactjs library and back-end with Nodejs.</p>
-                <p>Click on the projects link to know more of my
-                    expertise so far.</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="about-content">
+      <img src={Brian} />
+      <div className="about">
+        <h1>About me</h1>
+        <p>
+          My name is Brian Koome from Nairobi, Kenya and I completed my studies at African Leadership College studying Bsc. in Computing.
+        </p>
+        <p>
+          I am versatile and skilled in UX design, front-end development with HTML, CSS, SCSS, JavaScript and Reactjs library and back-end
+          with Nodejs.
+        </p>
+        <p>Click on the projects link to know more of my expertise so far.</p>
+      </div>
+    </div>
+  );
 }
