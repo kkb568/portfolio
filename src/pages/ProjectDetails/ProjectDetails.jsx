42367ba07d6daa053a29ae85a97afa0a81145f10
kkb568 <b.koome@alustudent.com>
2024-09-04 14:16:43 +0300

    Destructure projectObject state

--- src/pages/ProjectDetails/ProjectDetails.jsx
+++ src/pages/ProjectDetails/ProjectDetails.jsx
@@ -1,39 +1,47 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { projects } from "../data";
 import { useEffect, useState } from "react";
-import './ProjectDetails.scss'
+import "./ProjectDetails.scss";
 
 export default function ProjectDetails() {
-    const [projectObject, setProjectObject] = useState({
-        name: "",
-        imageUrl: "",
-        details: "",
-        siteUrl: ""
-    });
-    const { id } = useParams();
+  const [projectObject, setProjectObject] = useState({
+    name: "",
+    imageUrl: "",
+    details: "",
+    siteUrl: "",
+  });
+  const { id } = useParams();
 
-    useEffect(() => {
-        projects.filter(project => project.id == id).map((filteredProject) => {
-            setProjectObject(prevState => {
-                return {
-                    ...prevState,
-                    name: filteredProject.name,
-                    imageUrl: filteredProject.imageUrl,
-                    details: filteredProject.details,
-                    siteUrl: filteredProject.siteUrl
-                }
-            })
-        })
-    }, [])
+  const { name, imageUrl, details, siteUrl } = projectObject;
 
-    return (
-        <div className="projectDetails">
-            <img src={projectObject.imageUrl}/>
-            <div className="details">
-                <h3>{projectObject.name}</h3>
-                <p>{projectObject.details}</p>
-                <p>You can check the project site <a href={projectObject.siteUrl}>here</a></p>
-            </div>
-        </div>
-    )
+  useEffect(() => {
+    projects
+      .filter((project) => project.id === parseInt(id))
+      .map((filteredProject) => {
+        const { name, imageUrl, details, siteUrl } = filteredProject;
+
+        setProjectObject((prevState) => {
+          return {
+            ...prevState,
+            name,
+            imageUrl,
+            details,
+            siteUrl,
+          };
+        });
+      });
+  }, [id]);
+
+  return (
+    <div className="projectDetails">
+      <img src={imageUrl} />
+      <div className="details">
+        <h3>{name}</h3>
+        <p>{details}</p>
+        <p>
+          You can check the project site <a href={siteUrl}>here</a>
+        </p>
+      </div>
+    </div>
+  );
 }
