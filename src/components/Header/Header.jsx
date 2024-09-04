f3ea1474a77ffd4682c8c418ba88eec5cc75483a
kkb568 <b.koome@alustudent.com>
2024-09-04 16:21:03 +0300

    Add alt attibute to logo image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
-import { Link } from 'react-router-dom'
-import './Header.scss'
-import logo from '../../images/logo.png'
+import { Link } from "react-router-dom";
+import "./Header.scss";
+import logo from "../../images/logo.png";
 
 export default function Header() {
-    return (
-        <header>
-            <Link to="/"><img src={logo}/></Link>
-            <nav>
-                <Link to="/about">About</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      <Link to="/">
+        <img src={logo} alt="Logo combining letter b and k" />
+      </Link>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
 }
