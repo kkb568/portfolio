35cc3b75cb4ccef170f5fe8882d9f43cfb3bcf5e
kkb568 <b.koome@alustudent.com>
2024-09-04 14:20:17 +0300

    Format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Layout from "./components/Layout"
-import Home from "./pages/Home/Home"
-import About from "./pages/About/About"
-import Contact from "./pages/Contact/Contact"
-import Projects from "./pages/Projects/Projects"
-import ProjectDetails from "./pages/ProjectDetails/ProjectDetails"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Projects from "./pages/Projects/Projects";
+import ProjectDetails from "./pages/ProjectDetails/ProjectDetails";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Layout/>}>
-            <Route index element={<Home/>}/>
-            <Route path="about" element={<About/>}/>
-            <Route path="projects" element={<Projects/>}/>
-            <Route path="projects/:id" element={<ProjectDetails/>}/>
-            <Route path="contact" element={<Contact/>}/>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="projects/:id" element={<ProjectDetails />} />
+            <Route path="contact" element={<Contact />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,16 @@
-import './Home.scss'
+import "./Home.scss";
 
 export default function Home() {
-    return (
-        <div className="home-content">
-            <p className='emoji'>&#9995;</p>
-            <div className="content">
-                <h1>Hello and welcome</h1>
-                <p>Who am I? A young man passionate 
-                    and knowledgeable in UX design, 
-                    front and back-end development. Click on 
-                    the about link to know who exactly am I.</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="home-content">
+      <p className="emoji">&#9995;</p>
+      <div className="content">
+        <h1>Hello and welcome</h1>
+        <p>
+          Who am I? A young man passionate and knowledgeable in UX design, front and back-end development. Click on the about link to know
+          who exactly am I.
+        </p>
+      </div>
+    </div>
+  );
 }
