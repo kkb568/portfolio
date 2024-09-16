96c3beee31031e666d626cc1352a3dfe23b9138b
kkb568 <b.koome@alustudent.com>
2024-09-16 12:27:41 +0300

    Include home button in PageNotFound component

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import "./PageNotFound.scss";
 
 export default function PageNotFound() {
   return (
     <div className="pageNotFound">
       <h1>404</h1>
-      <p>Sorry, the page is not available.</p>
+      <p>Sorry to disappoint you but the page is not available.</p>
+      <br />
+      <Link to="/">Back to home</Link>
     </div>
   );
 }
